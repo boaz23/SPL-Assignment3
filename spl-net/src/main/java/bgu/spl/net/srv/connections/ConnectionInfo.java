96c4d356d94a552d26867019cdd1b6cb7cc5d988
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2020-01-05 21:11:24 +0200

    fixed a problem in the Connections interface where it was exposing objects it should not

--- spl-net/src/main/java/bgu/spl/net/srv/connections/ConnectionInfo.java
+++ spl-net/src/main/java/bgu/spl/net/srv/connections/ConnectionInfo.java
@@ -1,15 +1,21 @@
 package bgu.spl.net.srv.connections;
 
 public class ConnectionInfo<T> {
-    private ConnectionHandler<T> connectionHandler;
+    private final int connectionId;
+    private final ConnectionHandler<T> connectionHandler;
     private Object attachment;
 
-    public ConnectionInfo(ConnectionHandler<T> connectionHandler, Object attachment) {
+    public ConnectionInfo(int connectionId, ConnectionHandler<T> connectionHandler, Object attachment) {
+        this.connectionId = connectionId;
         this.connectionHandler = connectionHandler;
         this.attachment = attachment;
     }
 
-    public ConnectionHandler<T> getConnectionHandler() {
+    public int getConnectionId() {
+        return connectionId;
+    }
+
+    ConnectionHandler<T> getConnectionHandler() {
         return connectionHandler;
     }
 
