4ca78a5b4b40e8cdcb2407d6563f22eec9ff168e
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2020-01-08 18:45:07 +0200

    added frame methods and consts

--- StompClient/include/stomp/Frame.h
+++ StompClient/include/stomp/Frame.h
@@ -2,14 +2,52 @@
 #ifndef STOMPCLIENT_FRAME_H
 #define STOMPCLIENT_FRAME_H
 
+#include <string>
+#include <unordered_map>
+
 // TODO: add fields
 // TODO: add more subclasses
 class Frame {
+private:
+    std::string _messageType;
+    std::unordered_map<std::string, std::string> _headers;
+    std::string _body;
 
+public:
+    const static std::string RECEIPT_ID;
+
+    Frame();
+    Frame(std::string messageType, std::unordered_map<std::string, std::string> headers, std::string body);
+
+    std::string messageType();
+    void setMessageType(std::string messageType);
+
+    std::string body();
+    void setBody(std::string body);
+
+    std::unordered_map<std::string, std::string> headers();
+
+    std::string receiptId();
+    void setReceipt(std::string receiptId);
 };
 
 class ConnectFrame : public Frame {
+public:
+    const static std::string ACCEPTVERSION_HEADER;
+    const static std::string HOST_HEADER;
+    const static std::string LOGIN_HEADER;
+    const static std::string PASSCODE_HEADER;
 
+    ConnectFrame(std::string acceptVersion, std::string host, std::string login, std::string passcode);
+};
+
+class ConnectedFrame : public Frame {
+public:
+    const static std::string VERSION_HEADER;
+
+    ConnectedFrame(std::string version);
+
+    std::string version();
 };
 
 #endif //STOMPCLIENT_FRAME_H
--- StompClient/src/stomp/Frame.cpp
+++ StompClient/src/stomp/Frame.cpp
@@ -1,2 +1,11 @@
 
 #include "../../include/stomp/Frame.h"
+
+const std::string Frame::RECEIPT_ID = "receipt-id";
+
+const std::string ConnectFrame::ACCEPTVERSION_HEADER = "accept-version";
+const std::string ConnectFrame::HOST_HEADER = "host";
+const std::string ConnectFrame::LOGIN_HEADER = "login";
+const std::string ConnectFrame::PASSCODE_HEADER = "passcode";
+
+const std::string ConnectedFrame::VERSION_HEADER = "version";
